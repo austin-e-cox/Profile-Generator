ee79176c74056855eb63a0b64d1104f013d43965
Austin Cox <austin.ed.cox@gmail.com>
2020-02-12 00:26:37 -0800

    coded js to generate new html file while inserting data, need to generate pdf still and clean up the styles

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const axios = require('axios').default;
 const inquirer = require('inquirer');
 const pdf = require('html-pdf');
+const jsdom = require("jsdom");
+const { JSDOM }  = jsdom;
+
+const linkIds = new Set(["loc","prof","blog"]);
 
 const questions = [
     {
@@ -18,7 +22,7 @@ const questions = [
 
 async function getInputs(){
     let res = await inquirer.prompt(questions).then(function(response){
-        console.log(response);
+        //console.log(response);
         return response;
     });
     return res;
@@ -48,8 +52,53 @@ async function queryGithub(userName) {
       }
 }
 
+function editHtmlDoc(filePath,replaceData){
+    const outFilePath = "index2.html";
+    fs.readFile(filePath, 'utf8', function(err, data) {
+        if (err) throw err;
+        //console.log(data);
+        const dom = new JSDOM(data);
+        const document = dom.window.document;
+        var window = document.defaultView;
+        // set up jQuery
+        var $ = require('jquery')(window);
+        const bgColor = replaceData.color;
+        
+        // do all the things
+        for (let [key, value] of Object.entries(replaceData)){
+            if (key !== "color"){
+                if (linkIds.has(key))
+                    $(`#${key}`).attr("href",value);
+                else
+                    $(`#${key}`).text(value);
+                //console.log(`#${key}`)
+                //console.log($(`#${key}`).text());
+            }
+            else{
+                // background color
+                $(`#${key}`).css(`background-color`, value)
+            }
+        }
+
+        // cleanup
+        $(".jsdom").remove();
+
+        // write new file
+        //console.log( document.doctype.innerHTML + document.head.innerHTML + document.body.innerHTML);
+        var output = `<!DOCTYPE html>\n<html lang="en">\n<head>${document.head.innerHTML}</head>\n<body style="background-color: ${bgColor}">${document.body.innerHTML}</body>`;
+
+        fs.writeFile(outFilePath, output, function(err) {
+            if (err) throw err;
+            console.log(`Saved new html as `);
+        });
+    });
+}
+
+
 async function main(){
     let inputs = await getInputs();
+    //let bgColor="blue";
+    //let userName = "austin-e-cox";
     let bgColor = inputs.color;
     let userName = inputs.userName;
     let ghd = await queryGithub(userName);
@@ -67,9 +116,17 @@ async function main(){
         num_following: ghd.following,
         num_stars: ghd.num_stars
     }
-    console.log(minimalGithubData);//minimalGithubData);
+    //console.log(minimalGithubData);//minimalGithubData);
     //mapsSearchLoc = minimalGithubData.loc.replace(" ","+")
     //https://www.google.com/maps/place/Seattle,+WA,+USA/
+    const newLoc = `https://www.google.com/maps/place/${minimalGithubData.loc.replace(/ /g,"+")}`
+    minimalGithubData.loc = `${newLoc}`;
+    //console.log(`${minimalGithubData.loc.replace(/ /g,"+")}`)
+    // generate new dict of all data for web page
+    const myData = {...minimalGithubData, color: bgColor}
+    // log data in file
+    fs.writeFileSync("data.txt",JSON.stringify(myData,null,2));
+    editHtmlDoc("index.html",myData)
 }
 
 main()
